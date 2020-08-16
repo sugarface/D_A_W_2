47ba7edd57b57d801d95db751de84047e96be15f
hvtan1999 <67236470+hvtan1999@users.noreply.github.com>
2020-08-16 23:30:06 +0700

    change middleware

--- D.A/middlewares/writerRestricted.js
+++ D.A/middlewares/writerRestricted.js
@@ -1,9 +1,9 @@
-module.exports = (req,res,next)=>{
-    if(!req.user){
+module.exports = (req, res, next) => {
+    if (!req.user) {
         var retUrl = req.originalUrl;
         return res.redirect(`/account/login?retUrl=${retUrl}`);
     }
-    if(req.user.PhanQuyen !== '2' && req.user.PhanQuyen !== '1'){
+    if (req.user.PhanQuyen !== '2' && req.user.PhanQuyen !== '1') {
         return res.redirect('/404');
     }
     next();
